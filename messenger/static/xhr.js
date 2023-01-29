32f8b78285456b8312e77bd8b87e031f9f326958
Name <shi-666@yandex.ru>
2023-01-29 15:52:01 +0300

    js try

--- messenger/static/xhr.js
+++ messenger/static/xhr.js
@@ -1,3 +1,5 @@
+// это из вебинара
+
 const socket = new WebSocket('ws://127.0.0.1:8000');
 
 socket.onopen = function(e) {
@@ -14,19 +16,39 @@ socket.onmessage = function(event) {
   }
 };
 
+// это из E6.6. Асинхронный код — Promises, Async/Await, JSONP
+// Создаем promise
+const f1 = () => {
+    return new Promise((resolve, reject) => {
+      resolve("Успешное выполнение promise 1");
+    });
+  };
+  
+  const f2 = new Promise((resolve, reject) => {
+    resolve("Успешное выполнение promise 2");
+  });
+  
+  // Объявляем асинхронную функцию
+  const usePromise = async () => {
+    console.log('начало');
+    
+    // Дожидаемся получения результата от первой функции
+    const promiseResult1 = await f1();
+    console.log('promiseResult1', promiseResult1);
+    
+    // Дожидаемся получения результата промиса
+    const promiseResult2 = await f2;
+    console.log('promiseResult2', promiseResult2)
+    
+    console.log('конец');
+  };
+  
+  usePromise();
 
-const divSelectUsers = document.querySelector('.div_select_users');
-const divUser = document.querySelector('.div_user');
-const divRooms = document.querySelector('.div_rooms');
-const divChat = document.querySelector('.div_chat');
-
-let userLoggedId; 
-let currentRoom = ''; 
 
 
-socket.onerror = (evt) => document.querySelector('#message').innerText = evt[key];
-chatSocket.onerror = (evt) => document.querySelector('#message').innerText = evt[key];
 
+// мой код отсюда
 
 socket.onopen = () => load_users();
 function load_users() {
@@ -40,118 +62,83 @@ socket.onmessage = function(event) {
     if ('message' in data) {
         document.querySelector('#message').innerText = data.message;
     };
+    if ('MessageList' in data) {
+        if (userLogged) {
+            printChat(data);
+        };
+    };
     if ('UserList' in data) {
-        if (userLoggedId) {
-            viewUserCard(userLoggedId);
+        if (userLogged) {
+            viewUserCard(userLogged);
         } else {
             console.log('Пользователь не выбран');
             printUsers(data);
         };
     };
     if ('RoomList' in data) {
-        if (userLoggedId) {
+        if (userLogged) {
             printRooms(data);
-        };
-    };
-    if ('MessageList' in data) {
-        if (userLoggedId) {
-            printChat(data);
         };
     };
 };
 
 
+// Список пользователей. Сохрани хотя бы логику 
+// https://stackoverflow.com/questions/73978053/how-to-add-a-delete-button-for-newly-displayed-objects-in-an-array
+
 function printUsers(data) {
-    delete data.UserList;
-    let list = '';
+    let list = [];
     for (let key in data) {
         const newString = `<tr><td>${data[key]}</td>
-        <td><button onclick="userLogged(${key})">выбрать</button></td>
-        <td><button onclick="deleteUser(${key})">удалить</button></td>`;
+        <td><button onclick='userLogged(${key})'>выбрать</button></td>
+        <td><button onclick='deleteUser(${key})'>удалить</button></td>`;
         list = list + newString;
     };
     divSelectUsers.innerHTML = `<table> ${list}</table><br>`;
 };
 
-document.querySelector('.btn_create_user').addEventListener('click', () => {
-    let name = document.getElementById("input_user");
-    if (name.value !== "") {
-        socket.send(JSON.stringify({'create_user': name.value}));
-        console.log({'create_user': name.value});
-        name.value = "";
-    };
-});
-
+// удаление
 function deleteUser(id) {
     socket.send(JSON.stringify({'delete_user': id}));
     console.log({'delete_user': id});
 };
 
 
-function userLogged(userId) {
-    if (userLoggedId == undefined) {
-        document.querySelector('.div_main').removeChild(document.querySelector('.div_start'));
-    };
-    userLoggedId = userId;
-    viewUserCard(userId);
-};
-
-
-function viewUserCard(userId) {
+function viewUserProfile(userId) {
     fetch(domain + 'users/' + userId +'/')
         .catch(err => console.log(err))
         .then(response => response.json())
-        .then(result => printUserCard(result))
+        .then(result => printUserProfile(result))
     socket.send(JSON.stringify({'load': 'rooms'}));
 };
 
-// Вывод на страницу содержимого карточки юзера
-function printUserCard(item) {
+// Вывод профиля пользователя
+function printUserProfile(item) {
     if (item.room == null) {
-        room = "не выбрана";
+        room = 'Не выбрана';
     } else {
         let idRoom = item.room[item.room.length-2];
         room = listrooms[idRoom];
     };
 
     divUser.innerHTML = `
-    <div class="div">
+    <div class='div'>
         <img src="${item.avatar}">
         <br>
         <strong>Сменить аватарку:</strong><br>
-        <input id="avatar-input" type="file" accept="image/*"><br>
-        <button onclick="editAvatar(${item.id})">отправить</button>
+        <input id='avatar-input' type='file' accept='image/*'><br>
+        <button onclick='editAvatar(${item.id})'>Отправить</button>
         <p>ID: ${item.id}</p>
-        <p>Имя: ${item.name} <button onclick="changeUserName(${item.id})">изменить</button></p>
+        <p>Имя: ${item.name} <button onclick='changeUserName(${item.id})'>Изменить</button></p>
         <p>Комната в чате: ${room}</p>
-        <h4 class="message" id="message"></h4>
+        <h4 class='message' id='message'></h4>
     </div>
     `;
 };
 
-// Функция выгрузки на сервер аватарки юзера
-async function editAvatar(userId) {
-    const formData = new FormData();
-    let fileField = document.querySelector('#avatar-input');
-    if (fileField.files[0]) {
-        formData.append('avatar', fileField.files[0]);
-        try {
-            const response = await fetch(domain + 'users/' + userId +'/', {
-                method: 'PATCH',
-                body: formData
-            });
-            const result = await response.json();
-            console.log('Фото юзера сохранено:', JSON.stringify(result));
-        } catch (error) {
-            console.log('Все пропало!!!');
-            console.error('Ошибка:', error);
-        }
-        viewUserCard(userId);
-    } else {
-        console.log('Файл не выбран!');
-        document.querySelector('#message').innerText = '!!! файл не выбран';
-    };
-};
+// Функция загрузки аватарки..........
+// туть
+
 
 
 function changeUserName(userId) {
@@ -161,31 +148,17 @@ function changeUserName(userId) {
 };
 
 
+// комнаты как пользователи. Но я написала фигни в пользователях()
 function printRooms(data) {
     delete data.RoomList;
-    let list = '';
+    let list = [];
     for (let key in data) {
         const newString = `<tr><td><b>${data[key]}</b></td>
-        <td><button onclick="deleteRoom(${key})">удалить</button></td>
-        <td><button onclick="editRoom(${key})">изменить</button></td>
-        <td><button onclick="selectRoom(${key})">подключиться</button></td></tr>`;
+        <td><button onclick='deleteRoom(${key})'>Удалить</button></td>
+        <td><button onclick='editRoom(${key})'>Изменить</button></td>
+        <td><button onclick='selectRoom(${key})'>Подключиться</button></td></tr>`;
         list = list + newString;
     };
-    list = '<table>' + list + '</table><br>'
-    list = list + `<input type="text" id="input_room" name="name_new_room" size="22" placeholder="Введите имя новой комнаты"><br>`
-    list = list + `<button class="btn btn_new_room">Создать комнату</button>`
-    divRooms.innerHTML = list;
-
-
-    document.querySelector('.btn_new_room').addEventListener('click', () => {
-        let name = document.getElementById("input_room");
-        if (name.value !== "") {
-            socket.send(JSON.stringify({'create_room': name.value}));
-            console.log({'create_room': name.value});
-            name.value = "";
-        };
-    });
-};
 
 
 function deleteRoom(id) {
@@ -201,27 +174,9 @@ function editRoom(id) {
 };
 
 
-
-    document.querySelector('.btn_message').addEventListener('click', () => {
-        let message = document.getElementById("input_message");
-        if (message.value !== "") {
-            chatSocket.send(JSON.stringify({'usersendcommandroom': 'message', 'room_id': currentRoom, 'userid': userLoggedId, 'message': message.value}));
-            console.log({'usersendcommandroom': 'message', 'room_id': currentRoom, 'user': userLoggedId, 'message': message.value});
-            message.value = "";
-        };
-    });
-
-
-    chatSocket.onmessage = function(event) {
-        let data = JSON.parse(event.data);
-        console.log(data);
-        textarea.value += `${data['name']}: ${data['message']}\n`;
-    };
-
-
-    function selectRoom(id) {
-        socket.send(JSON.stringify({'load': 'messageList', 'newroom_id': id}));
-        console.log({'load': 'messageList', 'newroom_id': id});
-        chatSocket.send(JSON.stringify({'usersendcommandroom': 'roomselect', 'newroom_id': id, 'oldroom_id': currentRoom}));
-        currentRoom = id; 
-    };
+function selectRoom(id) {
+    socket.send(JSON.stringify({'load': 'messageList', 'newroom_id': id}));
+    console.log({'load': 'messageList', 'newroom_id': id});
+    chatSocket.send(JSON.stringify({'roomselect': 'newroom_id', id, 'oldroom_id': currentRoom}));
+    currentRoom = id; 
+};
